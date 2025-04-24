863dbf0dd85ce176c4618a6fd676fc3cd8cfe5e7
Victor Manuel <britoanicavictormanuel@gmail.com>
2025-04-23 20:55:05 -0600

    First commit: updaload new project with basic template and libreries

--- app/src/main/java/com/template/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/template/navigation/NavigationWrapper.kt
@@ -1,13 +1,11 @@
 package com.template.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.template.ui.home.HomeScreen
 import com.template.ui.login.LoginScreen
-import com.template.ui.pokemon.PokemonListScreen
 
 @Composable
 fun NavigationWrapper () {
@@ -22,10 +20,6 @@ fun NavigationWrapper () {
 
         composable<Home> {
             HomeScreen()
-        }
-
-        composable<PokemonList>{
-            PokemonListScreen()
         }
     }
 }
